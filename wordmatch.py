b8860d2d15c382f63f390fc4b0ac53600ac863c7
christiandavidfs <christiandavid.fs@gmail.com>
2021-08-08 19:57:36 -0300

    Add files via upload

---
+++ wordmatch.py
@@ -0,0 +1,36 @@
+
+#Type the sentence to be evaluated
+
+str = input()
+my_list = []
+
+def load_file():
+    #Load the words from the file
+
+    with open("File1.txt") as file:
+        for l in file:
+            my_list.append(l.strip())
+
+def matching():
+
+    #everything to Lowercase
+    a= [x.lower() for x in my_list]
+
+    #a=my_list
+    sentence=str.lower()
+
+
+    #review matches
+    allmatch= [x for x in a if x in sentence]
+
+    #save matches to variable
+    b=allmatch
+    # print(my_list)
+    #print(b)
+
+    #assert if matches mtach words and length
+    print (set(a) == set(b) and len(a) == len(b))
+
+
+load_file()
+matching()
